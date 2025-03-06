7a9be31e8e545a465748155519405a9c5185e59b
adilarundaya23 <168726534+adilarundaya23@users.noreply.github.com>
2025-03-06 11:17:09 +0700

    Update lengkap.py

--- lengkap.py
+++ lengkap.py
@@ -197,7 +197,7 @@ def admin_page():
         st.write("Selamat datang di halaman admin.")
     elif menu == "Live CCTV":
         st.title("Live CCTV")
-        cctv_url = "https://charitable-andorra-worker-votes.trycloudflare.com "
+        cctv_url = "https://hack-dealer-whether-remain.trycloudflare.com/"
         st.write(f"Tonton CCTV di [klik di sini]({cctv_url}) jika tidak muncul.")
         st.components.v1.html(
             f"""
